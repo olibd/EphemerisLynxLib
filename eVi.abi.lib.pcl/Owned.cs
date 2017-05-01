115ef45eb672df1e30d70b2dedc5984ffb106253
Olivier Brochu Dufour <olibd@outlook.com>
2017-05-01 17:13:03 -0400

    Added missing byte code to some classes to easy deployment

--- eVi.abi.lib.pcl/Owned.cs
+++ eVi.abi.lib.pcl/Owned.cs
@@ -13,6 +13,13 @@ namespace eVi.abi.lib.pcl
 
         public static string ABI = @"[{'constant':true,'inputs':[],'name':'owner','outputs':[{'name':'','type':'address'}],'payable':false,'type':'function'},{'inputs':[],'payable':false,'type':'constructor'}]";
 
+        public static string BYTE_CODE = "0x6060604052341561000c57fe5b5b33600060006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055505b5b60db8061005e6000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680638da5cb5b14603a575bfe5b3415604157fe5b60476089565b604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff16815600a165627a7a72305820cffe4025d922ec32d81f5d2429ba22264908c1a0dfb2d26dc0cc87755f4fab270029";
+
+        public static Task<string> DeployContractAsync(Web3 web3, string addressFrom,  HexBigInteger gas = null, HexBigInteger valueAmount = null) 
+        {
+            return web3.Eth.DeployContract.SendRequestAsync(ABI, BYTE_CODE, addressFrom, gas, valueAmount );
+        }
+
         private Contract contract;
 
         public OwnedService(Web3 web3, string address)
