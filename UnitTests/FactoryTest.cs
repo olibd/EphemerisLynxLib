dd3db27740bb3a55a319dec73a6d303d9e9ebaaa
Olivier Brochu Dufour <olibd@outlook.com>
2017-05-12 15:54:26 -0400

    Debbuging for contract deployment

--- UnitTests/FactoryTest.cs
+++ UnitTests/FactoryTest.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using eVi.abi.lib.pcl;
 using Nethereum.RPC.Eth.DTOs;
 using Nethereum.Hex.HexTypes;
+using System.Threading;
 
 namespace UnitTests
 {
@@ -27,10 +28,20 @@ namespace UnitTests
             launcher.Arguments = "--port 8545";
             //We configure embeded test rpc instance
             launcher.StartTestRPC();*/
+
+            Console.ReadLine();
+
             web3 = new Web3();
+
+			//Give time for the process start
+			Thread.Sleep(2000);
 
             //get first account
             addressFrom = (await web3.Eth.Accounts.SendRequestAsync())[0];
+
+            Console.ReadLine();
+
+            Console.WriteLine(addressFrom);
 
             //deploy the factory contract
             //string transactionHash = await FactoryService.DeployContractAsync(web3, addressFrom, new HexBigInteger(900000), new HexBigInteger(900000));
@@ -44,55 +55,55 @@ namespace UnitTests
             //Instantiate the factory
             factory = new FactoryService(web3, receipt.ContractAddress);*/
 
+
             var contractByteCode = OwnedService.BYTE_CODE;
-			//OwnedService ABI
-			//var abi = OwnedService.ABI;
+            //OwnedService ABI
+            var abi = OwnedService.ABI;
 
-			//Factory ABI
-			//var abi = FactoryService.ABI;
+            //Factory ABI
+            //var abi = FactoryService.ABI;
 
-			//no @ ABI
-			//var abi = "[{'constant':true,'inputs':[],'name':'owner','outputs':[{'name':'','type':'address'}],'payable':false,'type':'function'},{'inputs':[],'payable':false,'type':'constructor'}]";
+            //no @ ABI
+            //var abi = "[{'constant':true,'inputs':[],'name':'owner','outputs':[{'name':'','type':'address'}],'payable':false,'type':'function'},{'inputs':[],'payable':false,'type':'constructor'}]";
 
-			//abi form testrpc sample
-			var abi = @"[{""constant"":true,""inputs"":[],""name"":""getMultiplier"",""outputs"":[{""name"":""d"",""type"":""uint256""}],""type"":""function""},{""constant"":true,""inputs"":[],""name"":""contractName"",""outputs"":[{""name"":"""",""type"":""string""}],""type"":""function""},{""constant"":false,""inputs"":[{""name"":""a"",""type"":""uint256""}],""name"":""multiply"",""outputs"":[{""name"":""d"",""type"":""uint256""}],""type"":""function""},{""inputs"":[{""name"":""multiplier"",""type"":""uint256""}],""type"":""constructor""}]";
-			//abi form testrpc sample
-			//var abi = "[{'constant':true,'inputs':[],'name':'getMultiplier','outputs':[{'name':'d','type':'uint256'}],'type':'function'},{'constant':true,'inputs':[],'name':'contractName','outputs':[{'name':'','type':'string'}],'type':'function'},{'constant':false,'inputs':[{'name':'a','type':'uint256'}],'name':'multiply','outputs':[{'name':'d','type':'uint256'}],'type':'function'},{'inputs':[{'name':'multiplier','type':'uint256'}],'type':'constructor'}]";
-			//abi form testrpc sample
-			//var abi = "[{\"constant\":true,\"inputs\":[],\"name\":\"getMultiplier\",\"outputs\":[{\"name\":\"d\",\"type\":\"uint256\"}],\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"contractName\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"a\",\"type\":\"uint256\"}],\"name\":\"multiply\",\"outputs\":[{\"name\":\"d\",\"type\":\"uint256\"}],\"type\":\"function\"},{\"inputs\":[{\"name\":\"multiplier\",\"type\":\"uint256\"}],\"type\":\"constructor\"}]";
+            //abi form testrpc sample
+            //var abi = @"[{""constant"":true,""inputs"":[],""name"":""getMultiplier"",""outputs"":[{""name"":""d"",""type"":""uint256""}],""type"":""function""},{""constant"":true,""inputs"":[],""name"":""contractName"",""outputs"":[{""name"":"""",""type"":""string""}],""type"":""function""},{""constant"":false,""inputs"":[{""name"":""a"",""type"":""uint256""}],""name"":""multiply"",""outputs"":[{""name"":""d"",""type"":""uint256""}],""type"":""function""},{""inputs"":[{""name"":""multiplier"",""type"":""uint256""}],""type"":""constructor""}]";
+            //abi form testrpc sample
+            //var abi = "[{'constant':true,'inputs':[],'name':'getMultiplier','outputs':[{'name':'d','type':'uint256'}],'type':'function'},{'constant':true,'inputs':[],'name':'contractName','outputs':[{'name':'','type':'string'}],'type':'function'},{'constant':false,'inputs':[{'name':'a','type':'uint256'}],'name':'multiply','outputs':[{'name':'d','type':'uint256'}],'type':'function'},{'inputs':[{'name':'multiplier','type':'uint256'}],'type':'constructor'}]";
+            //abi form testrpc sample
+            //var abi = "[{\"constant\":true,\"inputs\":[],\"name\":\"getMultiplier\",\"outputs\":[{\"name\":\"d\",\"type\":\"uint256\"}],\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"contractName\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"a\",\"type\":\"uint256\"}],\"name\":\"multiply\",\"outputs\":[{\"name\":\"d\",\"type\":\"uint256\"}],\"type\":\"function\"},{\"inputs\":[{\"name\":\"multiplier\",\"type\":\"uint256\"}],\"type\":\"constructor\"}]";
 
-			//ABI for multiplication contract of abi-code-gen
-			//var abi = @"[{'constant':false,'inputs':[{'name':'a','type':'int256'}],'name':'multiply','outputs':[{'name':'r','type':'int256'}],'payable':false,'type':'function'},{'inputs':[{'name':'multiplier','type':'int256'}],'type':'constructor'},{'anonymous':false,'inputs':[{'indexed':true,'name':'a','type':'int256'},{'indexed':true,'name':'sender','type':'address'},{'indexed':false,'name':'result','type':'int256'}],'name':'Multiplied','type':'event'}]";
+            //ABI for multiplication contract of abi-code-gen
+            //var abi = @"[{'constant':false,'inputs':[{'name':'a','type':'int256'}],'name':'multiply','outputs':[{'name':'r','type':'int256'}],'payable':false,'type':'function'},{'inputs':[{'name':'multiplier','type':'int256'}],'type':'constructor'},{'anonymous':false,'inputs':[{'indexed':true,'name':'a','type':'int256'},{'indexed':true,'name':'sender','type':'address'},{'indexed':false,'name':'result','type':'int256'}],'name':'Multiplied','type':'event'}]";
 
-			//ABI for FuntionDTO contract of abi-code-gen
-			//var abi = @"[{'constant':false,'inputs':[{'name':'key','type':'bytes32'},{'name':'name','type':'string'},{'name':'description','type':'string'}],'name':'StoreDocument','outputs':[{'name':'success','type':'bool'}],'payable':false,'type':'function'},{'constant':true,'inputs':[{'name':'','type':'bytes32'},{'name':'','type':'uint256'}],'name':'documents','outputs':[{'name':'name','type':'string'},{'name':'description','type':'string'},{'name':'sender','type':'address'}],'payable':false,'type':'function'}]";
-			//abi generated from solcjs raw:
-			//var abi = "[{\"constant\":true,\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"type\":\"function\"},{\"inputs\":[],\"payable\":false,\"type\":\"constructor\"}]";
+            //ABI for FuntionDTO contract of abi-code-gen
+            //var abi = @"[{'constant':false,'inputs':[{'name':'key','type':'bytes32'},{'name':'name','type':'string'},{'name':'description','type':'string'}],'name':'StoreDocument','outputs':[{'name':'success','type':'bool'}],'payable':false,'type':'function'},{'constant':true,'inputs':[{'name':'','type':'bytes32'},{'name':'','type':'uint256'}],'name':'documents','outputs':[{'name':'name','type':'string'},{'name':'description','type':'string'},{'name':'sender','type':'address'}],'payable':false,'type':'function'}]";
+            //abi generated from solcjs raw:
+            //var abi = "[{\"constant\":true,\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"type\":\"function\"},{\"inputs\":[],\"payable\":false,\"type\":\"constructor\"}]";
 
-			//Factory abi generated from solcjs raw:
-			//var abi = "[{\"constant\":false,\"inputs\":[],\"name\":\"createID\",\"outputs\":[{\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"type\":\"function\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"_from\",\"type\":\"address\"},{\"indexed\":false,\"name\":\"_controllerAddress\",\"type\":\"address\"}],\"name\":\"ReturnIDController\",\"type\":\"event\"}]";
+            //Factory abi generated from solcjs raw:
+            //var abi = "[{\"constant\":false,\"inputs\":[],\"name\":\"createID\",\"outputs\":[{\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"type\":\"function\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"_from\",\"type\":\"address\"},{\"indexed\":false,\"name\":\"_controllerAddress\",\"type\":\"address\"}],\"name\":\"ReturnIDController\",\"type\":\"event\"}]";
 
 
-			//UroSharp Demo ABI
-			//var abi = "[{'constant':false,'inputs':[{'name':'score','type':'int256'}],'name':'setTopScore','outputs':[],'type':'function'},{'constant':true,'inputs':[{'name':'','type':'uint256'}],'name':'topScores','outputs':[{'name':'addr','type':'address'},{'name':'score','type':'int256'}],'type':'function'},{'constant':false,'inputs':[],'name':'getCountTopScores','outputs':[{'name':'','type':'uint256'}],'type':'function'},{'constant':true,'inputs':[{'name':'','type':'address'}],'name':'userTopScores','outputs':[{'name':'','type':'int256'}],'type':'function'}]";
-			
+            //UroSharp Demo ABI
+            //var abi = "[{'constant':false,'inputs':[{'name':'score','type':'int256'}],'name':'setTopScore','outputs':[],'type':'function'},{'constant':true,'inputs':[{'name':'','type':'uint256'}],'name':'topScores','outputs':[{'name':'addr','type':'address'},{'name':'score','type':'int256'}],'type':'function'},{'constant':false,'inputs':[],'name':'getCountTopScores','outputs':[{'name':'','type':'uint256'}],'type':'function'},{'constant':true,'inputs':[{'name':'','type':'address'}],'name':'userTopScores','outputs':[{'name':'','type':'int256'}],'type':'function'}]";
 
-            var transactionHash = await web3.Eth.DeployContract.SendRequestAsync(abi, contractByteCode, addressFrom, new HexBigInteger(900000), 7);
 
-			Assert.NotNull(transactionHash);
+            var transactionHash = await web3.Eth.DeployContract.SendRequestAsync(abi, contractByteCode, addressFrom, new HexBigInteger(900000));
+            Assert.NotNull(transactionHash);
         }
 
-		[TearDown]
+        [TearDown]
 		public void TearDown()
 		{
-            //launcher.StopTestRPC();
+            launcher.StopTestRPC();
 		}
 
         [Test()]
         public async Task TestCreateIDAsync()
         {
-            string transactionHash = await factory.CreateIDAsync(addressFrom);
-            Assert.NotNull(transactionHash);
+            //string transactionHash = await factory.CreateIDAsync(addressFrom);
+           //Assert.NotNull(transactionHash);
         }
 
 
---
+++ console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Nethereum.TestRPCRunner;
+using Nethereum.Web3;
+
+namespace console
+{
+    class MainClass
+    {
+
+        static string addressFrom;
+        static Web3 web3;
+        static TestRPCEmbeddedRunner launcher;
+		public static void Main(string[] args)
+		{
+
+            getAccountAsync().Wait();
+
+
+			launcher.StopTestRPC();
+		}
+
+
+        public static async System.Threading.Tasks.Task getAccountAsync()
+        {
+			launcher = new TestRPCEmbeddedRunner();
+			launcher.RedirectOuputToDebugWindow = true;
+			launcher.Arguments = "--port 8545";
+			launcher.StartTestRPC();
+
+			Console.ReadLine();
+
+			web3 = new Web3();
+            //get first account
+            addressFrom = (await web3.Eth.Accounts.SendRequestAsync())[0];
+        }
+    }
+}
